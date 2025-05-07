092ad9b62852c605979746b951da01ec8579a0ea
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2025-05-08 00:25:18 +0300

    Add necessary dependencies to project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "2.1.10"
+
+    kotlin("plugin.serialization") version "1.9.24"
 }
 
 group = "org.example"
@@ -15,7 +17,11 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.5.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
 
-
+    //ktor
+    val ktorVersion = "2.3.13"
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:1.5.6")
 
 }
 
