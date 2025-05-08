fbfc8ba0743ae9c0f61d4debcc1632e8f6458514
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2025-05-08 16:29:39 +0300

    Add safe API call helpers with error handling and response parsing

---
+++ src/main/kotlin/data/remote/utils/ApiCallHelpers.kt
@@ -0,0 +1,24 @@
+package data.remote.utils
+
+import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.bodyAsText
+import kotlinx.serialization.json.Json
+import logic.exception.ClothesSuggestException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
+
+inline fun safeApiCall(apiCall: () -> HttpResponse, ): HttpResponse {
+    return try {
+        apiCall()
+    } catch (e: UnknownHostException) {
+        throw ClothesSuggestException.NetworkException.NoInternetException
+    } catch (e: SocketTimeoutException) {
+        throw ClothesSuggestException.NetworkException.TimeoutException
+    } catch (e: Exception) {
+        throw ClothesSuggestException.NetworkException.UnknownNetworkException
+    }
+}
+
+suspend inline fun <reified T> getClassByResponse(response: HttpResponse, json: Json): T {
+    return json.decodeFromString<T>(response.bodyAsText())
+}
