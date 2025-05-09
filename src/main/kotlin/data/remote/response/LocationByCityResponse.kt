897a97674dcd2093b116f701935cb487525254fe
Shrouk Mohamed <96442266+ShroukMohamed16@users.noreply.github.com>
2025-05-09 11:14:05 +0300

    Created response classes

---
+++ src/main/kotlin/data/remote/response/LocationByCityResponse.kt
@@ -0,0 +1,32 @@
+package data.remote.response
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class LocationByCityResponse(
+    @SerialName("boundingbox")
+    val boundingbox: List<String?>?,
+    @SerialName("class")
+    val classX: String?,
+    @SerialName("display_name")
+    val displayName: String?,
+    @SerialName("importance")
+    val importance: Double?,
+    @SerialName("lat")
+    val lat: String?,
+    @SerialName("licence")
+    val licence: String?,
+    @SerialName("lon")
+    val lon: String?,
+    @SerialName("osm_id")
+    val osmId: String?,
+    @SerialName("osm_type")
+    val osmType: String?,
+    @SerialName("place_id")
+    val placeId: String?,
+    @SerialName("type")
+    val type: String?,
+
+)
