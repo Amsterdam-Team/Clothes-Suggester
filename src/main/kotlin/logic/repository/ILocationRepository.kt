ee3d2847a6058a980e515dd456a67151cba8f64d
Shrouk Mohamed <96442266+ShroukMohamed16@users.noreply.github.com>
2025-05-09 11:56:42 +0300

    Added location functions to repo interface and implementation

--- src/main/kotlin/logic/repository/ILocationRepository.kt
+++ src/main/kotlin/logic/repository/ILocationRepository.kt
@@ -1,11 +1,12 @@
 package logic.repository
 
+import data.remote.response.LocationByCityResponse
+import data.remote.response.LocationByIPAddressResponse
 import logic.entities.Location
 import org.example.logic.entities.IpLocationResponse
 
 interface ILocationRepository {
-    fun setUserLocation(location: Location)
-    fun getUserLocation(): Location
 
-    suspend fun getCurrentLocation() : IpLocationResponse
+    suspend fun getCurrentLocationByIPAddress(): Location
+    suspend fun getLocationByCityName(cityName:String): Location
 }
