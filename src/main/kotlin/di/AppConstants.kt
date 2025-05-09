f97ba043acbea7c4ba6a8ecfaf85752c5fc5ad0c
Shrouk Mohamed <96442266+ShroukMohamed16@users.noreply.github.com>
2025-05-09 11:35:46 +0300

    Updated di

--- src/main/kotlin/di/AppConstants.kt
+++ src/main/kotlin/di/AppConstants.kt
@@ -4,8 +4,15 @@ import org.koin.core.qualifier.named
 
 object AppConstants {
     const val WEATHER_API_BASE_URL = "https://api.open-meteo.com/v1/forecast"
+    const val LOCATION_BY_IP_API_BASE_URL = "http://ip-api.com/json"
+    const val LOCATION_BY_CITY_API_BASE_URL = "https://us1.locationiq.com/v1/search.php"
+    const val API_KEY = "pk.0aa96644f3d46d96744db9b5bc07822f"
+
 
     object Qualifiers {
         val WeatherApiBaseUrl = named("WeatherApiBaseUrl")
+        val LocationByIpApiBaseUrl = named("LocationByIpApiBaseUrl")
+        val LocationByCityApiBaseUrl = named("LocationByCityApiBaseUrl")
+
     }
 }
--- src/main/kotlin/di/DataSourceModule.kt
+++ src/main/kotlin/di/DataSourceModule.kt
@@ -4,18 +4,26 @@ import data.remote.dataSource.IWeatherDataSource
 import data.remote.dataSource.WeatherDataSourceImp
 import data.local.dataSource.ClothesDataSourceImp
 import data.local.dataSource.IClothesDataSource
-import data.local.dataSource.ILocationDataSource
-import data.local.dataSource.LocationDataSourceImp
+import data.remote.dataSource.ILocationRemoteDataSource
+import data.remote.dataSource.LocationRemoteDataSourceImp
 import org.koin.dsl.module
 
 val dataSourceModule = module {
-    single<ILocationDataSource> { LocationDataSourceImp() }
     single<IClothesDataSource> { ClothesDataSourceImp() }
     single<IWeatherDataSource> {
         WeatherDataSourceImp(
             client = get(),
             json = get(),
             baseUrl = get(AppConstants.Qualifiers.WeatherApiBaseUrl)
+        )
+    }
+    single<ILocationRemoteDataSource> {
+        LocationRemoteDataSourceImp(
+            client = get(),
+            json = get(),
+            ipBaseUrl = get(AppConstants.Qualifiers.LocationByIpApiBaseUrl),
+            cityBaseUrl = get(AppConstants.Qualifiers.LocationByCityApiBaseUrl),
+
         )
     }
 }
